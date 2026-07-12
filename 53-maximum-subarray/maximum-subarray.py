dade3143d78f076d264b246cafbbb089f2d8effb
Tejaswini Peddireddy <tejaswinipeddireddy16@gmail.com>
2026-07-12 20:19:33 +0530

    Time: 103 ms (22.64%) | Memory: 21.3 MB (45.85%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,8 +1,12 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         current = maximum = nums[0]
 
-        for num in nums[1:]:
-            current = max(num, current + num)
-            maximum = max(maximum, current)
+        for num in range(1,len(nums)):
+            current = max(nums[num], current + nums[num])
+            maximum= max(maximum, current)
         return maximum

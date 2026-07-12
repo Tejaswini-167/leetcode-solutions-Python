27c1f39f932d690c42ab8a12a476e7b56ffd6ef3
Tejaswini Peddireddy <tejaswinipeddireddy16@gmail.com>
2026-07-12 13:05:20 +0530

    Time: 6 ms (90.71%) | Memory: 32.2 MB (41.20%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,12 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
-        # return len(nums) != len(set(nums))
+        return len(nums) != len(set(nums))
 
-        s = set()
-        for num in nums:
-            if num in s:
-                return True
-            s.add(num)
-        return False
+        # s = set()
+        # for num in nums:
+        #     if num in s:
+        #         return True
+        #     s.add(num)
+        # return False
                     

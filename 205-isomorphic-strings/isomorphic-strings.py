83ada8e86cc2612db43206cb8f97258543e321b9
Tejaswini Peddireddy <tejaswinipeddireddy16@gmail.com>
2026-07-15 11:55:37 +0530

    Time: 4 ms (59.83%) | Memory: 19.3 MB (21.75%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -3,18 +3,27 @@ class Solution:
         map1 = {}
         map2 = {}
 
-        for i in range(len(s)):
+        # for i in range(len(s)):
 
-            if s[i] in map1:
-                if map1[s[i]] != t[i]:
-                    return False
-            else:
-                map1[s[i]] = t[i]
+        #     if s[i] in map1:
+        #         if map1[s[i]] != t[i]:
+        #             return False
+        #     else:
+        #         map1[s[i]] = t[i]
             
-            if t[i] in map2:
-                if map2[t[i]] != s[i]:
-                    return False
-            else:
-                map2[t[i]] = s[i]
+        #     if t[i] in map2:
+        #         if map2[t[i]] != s[i]:
+        #             return False
+        #     else:
+        #         map2[t[i]] = s[i]
 
+        # return True
+
+        for i in range(len(s)):
+            if s[i] not in map1:
+                map1[s[i]] = i
+            if t[i] not in map2:
+                map2[t[i]] = i
+            if map1[s[i]] != map2[t[i]]:
+                return False
         return True
